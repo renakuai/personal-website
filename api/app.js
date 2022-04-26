90b66aa736dd0c70f63f79727301b85cb401f999
Rena <rena.kuai@gmail.com>
2022-04-25 22:03:03 -0500

    React + Express version

--- api/app.js
+++ api/app.js
@@ -27,11 +27,16 @@ app.set('view engine', 'jade');
 // app.use code
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, './client/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // router setup
 app.use('/', indexRouter);
