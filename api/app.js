15b7840f84e0e391cd107054e0f7759c7c373c18
Rena <rena.kuai@gmail.com>
2022-04-25 22:13:02 -0500

    express react

--- api/app.js
+++ api/app.js
@@ -31,12 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 // router setup
 app.use('/', indexRouter);
